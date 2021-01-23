1f9e35a37a76a5b0337974eb4ac82c18d68548ee
kade-d <kldentel@bsu.edu>
2021-01-22 20:54:19 -0500

    Bonus Attempt

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,21 +1,19 @@
+import java.util.*;
+
 public class InsuranceCalculator {
 
     private InsuranceStrategy strategy;
 
-    public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
-    }
+    Map<Double, InsuranceStrategy> incomeBands = new LinkedHashMap<>() {{
+        put(10_000.0, new InsuranceStrategyLow());
+        put(30_000.0, new InsuranceStrategyMedium());
+        put(60_000.0, new InsuranceStrategyHigh());
+        put(Double.POSITIVE_INFINITY, new InsuranceStrategyVeryHigh());
+    }};
 
+    public double calculateInsurance(double income) {
+        Optional<Double> mapIncomeToKey = incomeBands.keySet().stream().filter(num -> income <= num).findFirst();
+        strategy = incomeBands.get(mapIncomeToKey.get());
+        return strategy.calculate(income);
+    }
 }
